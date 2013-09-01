aa9cc7c1c6ddf4dd78c625a56a7b508ea7e2b8e1
zsoltkiss <zsoltkiss71@gmail.com>
2013-09-01 02:47:11 +0200

    Introducing word card concept

---
+++ PocketVocab/LanguagePair.h
@@ -0,0 +1,18 @@
+//
+//  LanguagePair.h
+//  PocketVocab
+//
+//  Created by Kiss Rudolf Zsolt on 2013.08.31..
+//  Copyright (c) 2013 Kiss Rudolf Zsolt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LanguagePair : NSObject
+
+@property(nonatomic, strong) NSString *lang1;
+@property(nonatomic, strong) NSString *lang2;
+@property(nonatomic, strong) NSString *imageName1;
+@property(nonatomic, strong) NSString *imageName2;
+
+@end
---
+++ PocketVocab/LanguagePair.m
@@ -0,0 +1,13 @@
+//
+//  LanguagePair.m
+//  PocketVocab
+//
+//  Created by Kiss Rudolf Zsolt on 2013.08.31..
+//  Copyright (c) 2013 Kiss Rudolf Zsolt. All rights reserved.
+//
+
+#import "LanguagePair.h"
+
+@implementation LanguagePair
+
+@end
